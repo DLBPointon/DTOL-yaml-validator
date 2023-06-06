6a47de0f09eeed166bd45f3ed3d1635afa3510e1
DLBPointon <damonlbp@hotmail.co.uk>
2023-06-06 15:56:51 +0100

    Expanding plans

--- src/main.rs
+++ src/main.rs
@@ -53,6 +53,30 @@ struct DarwinYaml {
     stats: String,
 }
 
+fn darwin_function() {
+    println!("Hi im Darwin")
+}
+
+fn asg_function() {
+    println!("Hi im ASG")
+}
+
+fn genomeark_function()  {
+    println!("Hi im genomeArk")
+}
+
+fn erga_function()  {
+    println!("Hi im ERGA")
+}
+
+fn faculty_function() {
+    println!("Hi im faculty")
+}
+
+fn generate_template() {
+    println!("Do you need some help?")
+}
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     // Master list of allowed projects
     let valid_projects = ["darwin", "asg", "genomeark", "erga", "faculty"];
@@ -60,7 +84,10 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     // Convert CLI args into somthing usable
     let args: Vec<String> = env::args().collect();
     let file = fs::read_to_string(&args[2])?;
-
+    if &args[3] == "-gen" {
+        generate_template()
+    }
+ 
     // Use raw data to find project - Will fail if more than one
     let regex = Regex::new(r"projects:\r\n..-.([a-z]*)").unwrap();
     let regexed_project = regex.captures(&file).unwrap().get(1).map_or("", |m| m.as_str());
@@ -68,15 +95,15 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         panic!("Not a valid project! \nCheck format and make sure you pick a valid option.\n {:?}", valid_projects)
     }
 
-    println!("{}",regexed_project);
+    println!("CLI reports: {} | YAML reports: {}", &args[1], regexed_project);
 
-    match regexed_project {
-        "darwin" => println!("Reported Project is: {}", regexed_project),
-        "asg" => println!("Reported Project is: {}", regexed_project),
-        "genomeark" => println!("Reported Project is: {}", regexed_project),
-        "erga" => println!("Reported Project is: {}", regexed_project),
-        "faculty" => println!("Reported Project is: {}", regexed_project),
-        _ => println!("I dunno"),
+    match &*args[1] {
+        "darwin" => darwin_function(),
+        "asg" => asg_function(),
+        "genomeark" => genomeark_function(),
+        "erga" => erga_function(),
+        "faculty" => faculty_function(),
+        _ => panic!("What is {}, I don't know that one!", &args[1]),
     }
 
     Ok(())
